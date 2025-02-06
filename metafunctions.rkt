a8a08226011359a5b00904f6027b3b80cf2b1e60
AntFall <82494528+AntFall@users.noreply.github.com>
2025-02-06 11:51:44 -0500

    Added reify function

--- metafunctions.rkt
+++ metafunctions.rkt
@@ -1,12 +1,165 @@
 #lang racket
 (require redex)
+(require (rename-in minikanren
+                    (fresh fresh_)))
 (require "definitions.rkt")
 
-(define-metafunction L
-  unify-pair : any -> any
-  [(unify-pair (c t)) (== c t)])
+(provide reify to-json)
+
+(define (reify sub)
+  (let* ([underscore (λ (n) (string->symbol (string-append "_" (number->string n))))]
+         [freshen (λ (p) (underscore (car p)))]
+         [unify (λ (p) `(== ,(if (= (car p) 0) 'q (underscore (car p)))
+                            ,(if (number? (cadr p)) (underscore (cadr p)) (cadr p))))])
+    (car (eval `(run* (q) (fresh_ ,(map freshen sub) ,@(map unify sub)))))))
+                           
+
+(define (extract-name input-str)
+  (define re #px"^[x,r]:([a-zA-Z]+)") ;; (x or r):letters ; Stops at the <<...>>
+  (define matches (regexp-match re input-str))
+  (if matches
+      (second matches) 
+      ""))
 
 (define-metafunction L
-  reify : sub -> t
-  [(reify ((c t) ...))
-   ,(eval (term (car (run* (q) ,@(unify-pair (c t) ...)))))])
+  list->json : any -> any
+  [(list->json (t_1 : t_2))
+   ,(string-append (term (term->json t_1))
+                   ", "
+                   (term (list->json t_2)))]
+  [(list->json t) (term->json t)]
+  [(list->json (t)) (term->json t)]
+  [(list->json (t_1 t_2 ...))
+   ,(string-append (term (term->json t_1))
+                   ", "
+                   (term (list->json (t_2 ...))))])
+
+
+ 
+(define-metafunction L
+  term->json : t -> any
+  [(term->json c) ,(number->string (term c))]
+  [(term->json #t) "\"#t\""]
+  [(term->json #f) "\"#f\""]
+  [(term->json string) ,(string-append "\"" (term string) "\"")]
+  [(term->json x) ,(string-append "\"" (extract-name (symbol->string (term x))) "\"")]
+  [(term->json empty) "\"empty\""]
+  [(term->json (t_1 : t_2))
+   ,(string-append
+     "["
+     (term (list->json (t_1 : t_2)))
+     "]")])
+
+(define-metafunction L
+  sub->json : σ -> string
+  [(sub->json (state () c)) ""]
+  [(sub->json (state ((c t)) c_1))
+   ,(string-append
+    "{\"key\": " (number->string (term c))
+    ", \"value\": " (term (term->json t)) "}")] 
+  [(sub->json (state ((c t) (c_1 t_1) ...) c_2))
+   ,(string-append
+    "{\"key\": " (number->string (term c))
+    ", \"value\": " (term (term->json t)) "}, "
+    (term (sub->json (state ((c_1 t_1) ...) c_2))))])
+
+(define-metafunction L
+  goal->json : g -> string
+  [(goal->json ⊤)
+   "{\"name\": \"Succeed\"}"]
+
+  [(goal->json (t_1 =? t_2))
+   ,(let* ([left-json (term (term->json  t_1))]
+           [right-json (term (term->json t_2))])
+      (string-append
+       "{\"name\": \"Unify\", "
+       "\"left\": " left-json ", "
+       "\"right\": " right-json "}"))]
+
+  [(goal->json (r t ...))
+   ,(let* ([rel-name (extract-name (symbol->string (term r)))]
+           [args-json (term (list->json (t ...)))])
+      (string-append
+       "{\"name\": \"Rel-Call\", "
+       "\"rel\": \"" rel-name "\", "
+       "\"args\": [" args-json "]}"))]
+
+  [(goal->json (g_1 ∨ g_2))
+   ,(let* ([left-json (term (goal->json g_1))]
+           [right-json (term (goal->json g_2))])
+      (string-append
+       "{\"name\": \"Goal-Disj\", "
+       "\"children\": [" left-json ", "
+                         right-json "]}"))]
+
+  [(goal->json (g_1 ∧ g_2))
+   ,(let* ([left-json (term (goal->json g_1))]
+           [right-json (term (goal->json g_2))])
+      (string-append
+       "{\"name\": \"Goal-Conj\", "
+       "\"children\": [" left-json ", "
+                         right-json "]}"))]
+
+  [(goal->json (∃ d g))
+   ,(let* ([var-name  (term (list->json d))]
+           [goal-json (term (goal->json g))])
+      (string-append
+       "{\"name\": \"Fresh\", "
+       "\"vars\": [" var-name "], "
+       "\"children\": [" goal-json "]}"))])
+
+(define-metafunction L
+  to-json : s -> string
+  [(to-json ())
+   "{\"name\": \"Empty\"}"]
+
+  [(to-json (g σ))
+   ,(let* ([goal-json (term (goal->json g))]
+           [sigma-json (term (sub->json σ))])
+      (string-append
+       (substring goal-json 0 (sub1 (string-length goal-json))) ", "
+       "\"sub\": [" sigma-json "]}"))]
+
+  [(to-json (s_1 +-> s_2))
+   ,(let* ([left-json (term (to-json s_1))]
+           [right-json (term (to-json s_2))])
+      (string-append
+       "{\"name\": \"+->\", "
+       "\"children\": [" left-json ", "
+                         right-json "]}"))]
+
+  [(to-json (s_1 <-+ s_2))
+   ,(let* ([left-json (term (to-json s_1))]
+           [right-json (term (to-json s_2))])
+      (string-append
+       "{\"name\": \"<-+\", "
+       "\"children\": [" left-json ", "
+                         right-json "]}"))]
+
+  [(to-json ((⊤ σ) + s))
+   ,(let* ([sub-json (term (sub->json σ))]
+           [rest-json (term (to-json s))])
+      (string-append
+       "{\"name\": \"Answer\", "
+       "\"sub\": [" sub-json "], "
+       "\"children\": [" rest-json "]}"))]
+
+  [(to-json (s × g))
+   ,(let* ([left-json (term (to-json s))]
+           [right-json (term (goal->json g))])
+      (string-append
+       "{\"name\": \"Conjunction\", "
+       "\"children\": [" left-json ", " right-json "]}"))]
+
+  [(to-json (delay s))
+   ,(let* ([tree-json (term (to-json s))])
+      (string-append
+       "{\"name\": \"Delay\", "
+       "\"children\": [" tree-json "]}"))])
+
+(define-metafunction L
+  prog->tree : p -> e
+  [(prog->tree (prog Γ e)) e])
+
+
+
