53e6e31334b798627e0ab71726ed408f2cdc326a
brysenPfingsten <brysen.pfingsten@gmail.com>
2025-06-10 02:24:02 -0400

    Defuntionalized Version. Added back normal cps version.

--- Misc/dmitry-sos/sos-cps-df.rkt
+++ Misc/dmitry-sos/sos-cps-df.rkt
@@ -13,120 +13,193 @@
 (struct fresh (x g) #:transparent)
 (struct relcall (name terms) #:transparent)
 
-(define (walk/k t s k)
+(struct init:k (f*))
+
+(define (apply-k k v)
+  (match k
+    [(subst-term:car var* val* term* k*)
+     (subst-term/k var* val* (cdr term*) (subst-term:cdr v k*))]
+
+    [(subst-term:cdr a* k*)
+     (apply-k k* (cons a* v))]
+
+    [(subst-terms:car var* val* lst* k*)
+     (subst-terms/k var* val* (cdr lst*) (subst-terms:cdr v k*))]
+
+    [(subst-terms:cdr a* k*)
+     (apply-k k* (cons a* v))]
+
+    [(substitute:==/t1 fresh-var* replace-var* t2* k*)
+     (subst-term/k fresh-var* replace-var* t2*
+                   (substitute:==/t2 v k*))]
+
+    [(substitute:==/t2 t1* k*)
+     (apply-k k* (== t1* v))]
+
+    [(substitute:gor/g1 fresh-var* replace-var* g2* k*)
+     (substitute/k fresh-var* replace-var* g2*
+                   (substitute:gor/g2 v k*))]
+
+     [(substitute:gor/g2 g1* k*)
+      (apply-k k* (gor g1* v))]
+
+    [(substitute:gand/g1 fresh-var* replace-var* g2* k*)
+     (substitute/k fresh-var* replace-var* g2*
+                   (substitute:gand/g2 v k*))]
+
+     [(substitute:gand/g2 g1* k*)
+      (apply-k k* (gand g1* v))]
+
+     [(substitute:fresh x* k*)
+      (apply-k k* (fresh x* v))]
+
+     [(substitute:relcall name* k*)
+      (apply-k k* (relcall name* v))]
+
+     [(foldr:k args* terms* k*)
+      (foldr/k args* terms* v k*)]
+
+     [(invoke:k terms* k*)
+      (define args (car v))
+      (define goal (cdr v))
+      (foldr/k args terms* goal k*)]
+
+     [(dmitry:fresh s* n* k*)
+      (apply-k k* (list (trip v s* (add1 n*)) #f))]
+
+     [(dmitry:invoke s* n* k*)
+      (apply-k k* (list (trip v s* n*) #f))]
+
+     [(dmitry:tor t2* k*)
+      (match v
+        [(list (mt) #f)
+        (displayln "DisjStop")
+        (apply-k k* `(,t2* #f))]
+
+        [(list (mt) b)
+        (displayln "DisjStopAns")
+        (apply-k k* `(,t2* ,b))]
+
+        [(list s #f)
+        (displayln "DisjStep")
+        (apply-k k* `(,(tor t2* s) #f))]
+        [(list s b)
+        (displayln "DisjStepAns")
+        (apply-k k* `(,(tor t2* s) ,b))])]
+
+     [(dmitry:tand g* k*)
+        (match v
+          [(list (mt) #f)
+          (displayln "ConjStop")
+          (apply-k k* `(,(mt) #f))]
+  
+          [(list (mt) `(,subst ,n))
+          (displayln "ConjStopAns")
+          (apply-k k* `(,(trip g* subst n) #f))]
+
+          [(list s #f)
+          (displayln "ConjStep")
+          (apply-k k* `(,(tand s g*) #f))]
+
+          [(list t `(,subst ,n))
+          (displayln "ConjStepAns")
+          (apply-k k* `(,(tor (trip g* subst n) (tand t g*)) #f))])]
+     
+	[(init:k f*) (f* v)]))
+
+(define (walk t s)
   (if (not (number? t))
-      (k t)
+      t
       (match s
         [(cons (list v t^) _) 
          #:when (eq? t v)
-         (walk/k t^ s k)]
-        [(cons _ s^) (walk/k t s^ k)]
-        [_ (k t)])))
+         (walk t^ s)]
+        [(cons _ s^) (walk t s^)]
+        [_ t])))
 
 
-(define (occurs?/k v t s k)
+(define (occurs? v t s)
   (match t 
-    [(cons t1 t2) 
-     (walk/k t1 s
-             (λ (t1^)
-               (walk/k t2 s
-                       (λ (t2^)
-                         (occurs?/k v t1^ s
-                                    (λ (b1)
-                                      (occurs?/k v t2^ s
-                                                 (λ (b2)
-                                                   (k (or b1 b2))))))))))]
-    [t (walk/k t s
-               (λ (t^)
-                 (k (eq? v t^))))]))
+    [(cons t1 t2) (or (occurs? v (walk t1 s) s)
+                      (occurs? v (walk t2 s) s))]
+    [t (eq? v (walk t s))]))
 
 
-(define (extend/k v t s k)
-  (occurs?/k v t s
-             (λ (b)
-               (if b
-                   (k #f)
-                   (k (cons (list v t) s))))))
+(define (extend v t s)
+  (if (occurs? v t s)
+      #f
+      (cons (list v t) s)))
 
 
-(define (unify/k t1 t2 s k)
-  (walk/k t1 s
-          (λ (t1^)
-            (walk/k t2 s
-                    (λ (t2^)
-                      (unify-help/k t1^ t2^ s k))))))
+(define (unify t1 t2 s)
+  (let* ([t1^ (walk t1 s)]
+         [t2^ (walk t2 s)])
+    (unify-help t1^ t2^ s)))
 
 
-(define (unify-help/k t1 t2 s k)
+(define (unify-help t1 t2 s)
   (match (list t1 t2)
-    [(list t t) (k s)]
+    [(list t t) s]
     [(list v t) #:when (number? v)
-                (extend/k v t s k)]
+                (extend v t s)]
     [(list t v) #:when (number? v)
-                (extend/k v t s k)]
+                (extend v t s)]
     [(list (cons t1a t1b) (cons t2a t2b))
-     (unify/k t1a t2a s
-              (λ (s^)
-                (unify/k t1b t2b s^ k)))]
-    [(list _ _)  (k #f)]))
+     (define s^ (unify t1a t2a s))
+     (unify t1b t2b s^)]
+    [(list _ _)  #f]))
 
+
+(struct subst-term:car (var* val* term* k*))
+(struct subst-term:cdr (a* k*))
 
 (define (subst-term/k var val term k)
-  (cond 
+  (cond
     [(cons? term)
-     (subst-term/k var val (car term)
-                   (λ (a)
-                     (subst-term/k var val (cdr term)
-                                   (λ (d)
-                                     (k (cons a d))))))]
-    [(equal? term var) (k val)]
-    [else (k term)]))
+     (subst-terms/k var val (car term) (subst-term:car var val term k))]
+    [(equal? term var) (apply-k k val)]
+    [else (apply-k k term)]))
 
+(struct subst-terms:car (var* val* lst* k*))
+(struct subst-terms:cdr (a* k*))
 
 (define (subst-terms/k var val lst k)
   (if (null? lst)
-      (k '())
-      (subst-term/k var val (car lst)
-                    (λ (a^)
-                      (subst-terms/k var val (cdr lst)
-                                     (λ (d^)
-                                       (k (cons a^ d^))))))))
+      (apply-k k '())
+      (subst-term/k var val (car lst) (subst-terms:car var val lst k))))
 
+(struct substitute:==/t1 (fresh-var* replace-var* t2* k*))
+(struct substitute:==/t2 (t1* k*))
+(struct substitute:gor/g1 (fresh-var* replace-var* g2* k*))
+(struct substitute:gor/g2 (g1* k*))
+(struct substitute:gand/g1 (fresh-var* replace-var* g2* k*))
+(struct substitute:gand/g2 (g1* k*))
+(struct substitute:fresh (x* k*))
+(struct substitute:relcall (name* k*))
 
 (define (substitute/k fresh-var replace-var goal k)
   (match goal
     [(== t1 t2)
      (subst-term/k fresh-var replace-var t1
-                   (λ (t1^)
-                     (subst-term/k fresh-var replace-var t2
-                                   (λ (t2^)
-                                     (k (== t1^ t2^))))))]
+                   (substitute:==/t1 fresh-var replace-var t2 k))]
 
     [(gor g1 g2)
      (substitute/k fresh-var replace-var g1
-                   (λ (g1^)
-                     (substitute/k fresh-var replace-var g2
-                                   (λ (g2^)
-                                     (k (gor g1^ g2^))))))]
+                   (substitute:gor/g1 fresh-var replace-var g2 k))]
 
     [(gand g1 g2)
      (substitute/k fresh-var replace-var g1
-                   (λ (g1^)
-                     (substitute/k fresh-var replace-var g2
-                                   (λ (g2^)
-                                     (k (gand g1^ g2^))))))]
+                   (substitute:gand/g1 fresh-var replace-var g2 k))]
 
     [(fresh x g)
      (if (eqv? x fresh-var)
-         (k goal)
+         (apply-k k goal)
          (substitute/k fresh-var replace-var g
-                       (λ (g^)
-                         (k (fresh x g^)))))]
+                       (substitute:fresh x k)))]
 
     [(relcall name terms)
      (subst-terms/k fresh-var replace-var terms
-                    (λ (terms^)
-                      (k (relcall name terms^))))]))
+                    (substitute:relcall name k))]))
 
 
 (define sameo (rel 'sameo '(x y) (== 'x 'y)))
@@ -138,165 +211,133 @@
   (match env
     [(cons (rel rname args goal) rest)
      #:when (eqv? name rname)
-     (k `(,args . ,goal))]
+     (apply-k k `(,args . ,goal))]
 
     [(cons _ rest)
      (lookup-rel/k name rest k)]
 
     [_ (error "Relation not found")]))
 
+(struct foldr:k (args* terms* k*))
+
 (define (foldr/k args terms goal k)
   (if (null? args)
-      (k goal)
+      (apply-k k goal)
       (substitute/k (car args) (car terms) goal
-                    (λ (goal^)
-                      (foldr/k (cdr args) (cdr terms) goal^ k)))))
+                    (foldr:k (cdr args) (cdr terms) k))))
+
+(struct invoke:k (terms* k*))
 
 (define (invoke/k name terms k)
   (lookup-rel/k name rels
-                (λ (result)
-                  (define args (car result))
-                  (define goal (cdr result))
-                  (foldr/k args terms goal k))))
+                (invoke:k terms k)))
 
+(struct dmitry:fresh (s* n* k*))
+(struct dmitry:invoke (s* n* k*))
+(struct dmitry:tor (t2* k*))
+(struct dmitry:tand (g* k*))
 
 (define (dmitry/k t k)
   (match t
     [(trip (== t1 t2) s n)
-     (unify/k t1 t2 s 
-              (λ (s^)
+     (let ((s^ (unify t1 t2 s)))
                 (cond
                   [s^ (displayln "UnifySuccess")
-                      (k (list (mt) `(,s^ ,n)))]
+                      (apply-k k (list (mt) `(,s^ ,n)))]
                   [else (displayln "UnifyFail")
-                        (k (list (mt) #f))])))]
+                        (apply-k k (list (mt) #f))]))]
 
     [(trip (gor g1 g2) s n)
      (displayln "Disj")
-     (k (list (tor (trip g1 s n) (trip g2 s n)) #f))]
+     (apply-k k (list (tor (trip g1 s n) (trip g2 s n)) #f))]
 
     [(trip (gand g1 g2) s n)
      (displayln "Conj")
-     (k (list (tand (trip g1 s n) g2) #f))]
+     (apply-k k (list (tand (trip g1 s n) g2) #f))]
 
     [(trip (fresh x g) s n)
      (displayln "Fresh")
      (substitute/k x n g
-                   (λ (g^)
-                     (k (list (trip g^ s (add1 n)) #f))))]
+                   (dmitry:fresh s n k))]
 
     [(trip (relcall name ts) s n)
      (displayln "Invoke")
      (invoke/k name ts
-               (λ (g^)
-                 (k (list (trip g^ s n) #f))))]
+               (dmitry:invoke s n k))]
 
     [(tor t1 t2)
      (dmitry/k t1
-               (λ (t1^)
-                 (match t1^
-                   [(list (mt) #f)
-                    (displayln "DisjStop")
-                    (k `(,t2 #f))]
-
-                   [(list (mt) b)
-                    (displayln "DisjStopAns")
-                    (k `(,t2 ,b))]
-
-                   [(list s #f)
-                    (displayln "DisjStep")
-                    (k `(,(tor t2 s) #f))]
-
-                   [(list s b)
-                    (displayln "DisjStepAns")
-                    (k `(,(tor t2 s) ,b))])))]
-
+               (dmitry:tor t2 k))]
     [(tand t g)
      (dmitry/k t
-               (λ (t^)
-                 (match t^
-                   [(list (mt) #f)
-                    (displayln "ConjStop")
-                    (k `(,(mt) #f))]
-           
-                   [(list (mt) `(,subst ,n))
-                    (displayln "ConjStopAns")
-                    (k `(,(trip g subst n) #f))]
-
-                   [(list s #f)
-                    (displayln "ConjStep")
-                    (k `(,(tand s g) #f))]
-
-                   [(list t `(,subst ,n))
-                    (displayln "ConjStepAns")
-                    (k `(,(tor (trip g subst n) (tand t g)) #f))])))]
+               (dmitry:tand g k))]
     ))
                    
-(define end/k (lambda (x) x))
 
 (define-test-suite MK-HELPERS
   (test-case "Non-Numeric Term Returns Itself"
-             (check-equal? (walk/k 'dog '() end/k) 'dog)
-             (check-equal? (walk/k "dog" '() end/k) "dog")
-             (check-equal? (walk/k #t '() end/k) #t)
-             (check-equal? (walk/k (cons 0 1) '() end/k) (cons 0 1)))
+             (check-equal? (walk 'dog '()) 'dog)
+             (check-equal? (walk "dog" '()) "dog")
+             (check-equal? (walk #t '()) #t)
+             (check-equal? (walk (cons 0 1) '()) (cons 0 1)))
 
   (test-case "Numeric Term Walks To Correct Value"
-             (check-equal? (walk/k 0 `((0 dog)) end/k) 'dog)
-             (check-equal? (walk/k 0 `((0 1) (1 2) (2 dog)) end/k) 'dog)
-             (check-equal? (walk/k 0 `((0 (1 2)) (1 "bear") (2 "bird")) end/k) (list 1 2)))
+             (check-equal? (walk 0 `((0 dog))) 'dog)
+             (check-equal? (walk 0 `((0 1) (1 2) (2 dog))) 'dog)
+             (check-equal? (walk 0 `((0 (1 2)) (1 "bear") (2 "bird"))) (list 1 2)))
 
   (test-case "Numeric Term Not Bound Returns Itself"
-             (check-equal? (walk/k 0 '() end/k) 0)
-             (check-equal? (walk/k 5 '((0 1) (1 2) (2 3)) end/k) 5))
+             (check-equal? (walk 0 '()) 0)
+             (check-equal? (walk 5 '((0 1) (1 2) (2 3))) 5))
 
   (test-true "Logic Var Occurs In Itself"
-             (occurs?/k 0 0 '() end/k))
+             (occurs? 0 0 '()))
 
   (test-false "Logic Var Does Not Appear In Ground Atomic Term"
-              (occurs?/k 0 "not here" '() end/k))
+              (occurs? 0 "not here" '()))
 
   (test-true "Logic Var Appears Nested In A List"
-             (occurs?/k 0 (cons (cons 1 2) (cons (cons 0 5) 12)) '() end/k))
+             (occurs? 0 (cons (cons 1 2) (cons (cons 0 5) 12)) '()))
 
   (test-true "Logic Var Appears In A Walked Term"
-             (occurs?/k 0 1 '((1 0)) end/k))
+             (occurs? 0 1 '((1 0))))
 
   (test-false "Logic Var Does Not Appear In A List"
-              (occurs?/k 0 (cons 1 2) '() end/k))
+              (occurs? 0 (cons 1 2) '()))
 
   (test-case "Substitution Is Extended When Occurs Check Passes"
-             (check-false (occurs?/k 0 "dog" '() end/k))
-             (check-equal? (extend/k 0 "dog" '() end/k) '((0 "dog"))))
+             (check-false (occurs? 0 "dog" '()))
+             (check-equal? (extend 0 "dog" '()) '((0 "dog"))))
 
   (test-case "Extending Substitution Fails When Occurs Check Fails"
-             (check-true (occurs?/k 0 0 '() end/k))
-             (check-false (extend/k 0 0 '() end/k)))
+             (check-true (occurs? 0 0 '()))
+             (check-false (extend 0 0 '())))
 
   (test-case "Unifying Two of the Same Terms Returns The Original Substitution"
-             (check-equal? (unify/k 0 0 '() end/k) '())
-             (check-equal? (unify/k "dog" "dog" '() end/k) '()))
+             (check-equal? (unify 0 0 '()) '())
+             (check-equal? (unify "dog" "dog" '()) '()))
 
   (test-case "Unifying A Logic Var And A Term Extends The Substitution"
-             (check-equal? (unify/k 0 "dog" '() end/k) '((0 "dog")))
-             (check-equal? (unify/k "dog" 0 '() end/k) '((0 "dog")))
-             (check-equal? (unify/k 0 1 '((1 "cat")) end/k) '((0 "cat") (1 "cat"))))
+             (check-equal? (unify 0 "dog" '()) '((0 "dog")))
+             (check-equal? (unify "dog" 0 '()) '((0 "dog")))
+             (check-equal? (unify 0 1 '((1 "cat"))) '((0 "cat") (1 "cat"))))
 
   (test-case "Unifying Pairs Works"
-             (check-equal? (unify/k (cons 0 "bear") (cons "eagle" 1) '() end/k)
+             (check-equal? (unify (cons 0 "bear") (cons "eagle" 1) '())
                            '((1 "bear") (0 "eagle")))
-             (check-equal? (unify/k (cons 0 1) (cons 2 3) '((0 "dog") (1 "cat") (2 "dog") (3 "cat")) end/k)
+             (check-equal? (unify (cons 0 1) (cons 2 3) '((0 "dog") (1 "cat") (2 "dog") (3 "cat")))
                            '((0 "dog") (1 "cat") (2 "dog") (3 "cat"))))
 
   (test-case "Unifying Two Different Terms Fails"
-             (check-false (unify/k "dog" "cat" '() end/k))
-             (check-false (unify/k 0 1 '((0 "dog") (1 #t)) end/k)))
+             (check-false (unify "dog" "cat" '()))
+             (check-false (unify 0 1 '((0 "dog") (1 #t)))))
   )
+
 
 (define (dmitry-capture-output prog)
   (let ([out (open-output-string)])
     (parameterize ([current-output-port out])
-      (define res (dmitry/k prog end/k))
+      (define res (dmitry/k prog (init:k identity)))
       (values res (get-output-string out)))))
 
 (define-test-suite DMITRY
@@ -509,4 +550,7 @@
                                             (== "ghi" "ghi")))
                                  #f))
              (check-equal? out "UnifySuccess\nDisjStopAns\nConjStepAns\n"))
-  ) 
+  )
+
+(module+ test
+  (run-test DMITRY))
