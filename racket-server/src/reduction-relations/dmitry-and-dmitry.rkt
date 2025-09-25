76fa3d8baeb9adbf255b41e907bc8277b1ffa936
Brysen Pfingsten <brysen.pfingsten@gmail.com>
2025-09-25 11:59:50 -0400

    Dmitry semantics adapted to new program style

--- racket-server/src/reduction-relations/dmitry-and-dmitry.rkt
+++ racket-server/src/reduction-relations/dmitry-and-dmitry.rkt
@@ -13,10 +13,25 @@
 (define (step-once prog)
   (apply-reduction-relation/tag-with-names dmitry-and-dmitry (term ,prog)))
 
-
 (define dmitry-and-dmitry
-  (reduction-relation L 
+  (reduction-relation L
                       #:domain (side-condition (name prog p) (judgment-holds (closed-program? prog)))
+
+                      [--> ((in-hole Ex ((r_1 t ... o) σ))
+                            ((r_0 (x_0 ...) g_0) ... (r_1 (x_1 ...) g_1) (r_2 (x_2 ...) g_2) ...))
+                           ((in-hole Ex (∂ ((substitute g_1 (x_1 t) ...) σ) #f))
+                            ((r_0 (x_0 ...) g_0) ... (r_1 (x_1 ...) g_1) (r_2 (x_2 ...) g_2) ...))
+                           "Invoke"]
+
+                      [--> (e_1 Γ)
+                           (e_2 Γ)
+                        (side-condition (not (null? (apply-reduction-relation red-tree (term e_1)))))
+                        (where e_2 ,(car (apply-reduction-relation red-tree (term e_1))))
+                        (computed-name (caar (apply-reduction-relation/tag-with-names red-tree (term e_1))))]))
+
+(define red-tree
+  (reduction-relation L 
+                      #:domain e
 
                       [==> ((t_1 =? t_2 o) (state sub _ _ _))
                            (∂ () #f)
@@ -41,11 +56,6 @@
                            (∂ ((substitute g ,@(term (fresh-sub c x))) (state sub ,(+ 1 (term c)) trail o)) #f)
                            "Fresh"]
 
-                      [--> (prog ((r_0 (x_0 ...) g_0) ... (r_1 (x_1 ...) g_1) (r_2 (x_2 ...) g_2) ...) 
-                                  (in-hole Ev (in-hole Es ((r_1 t ... o) σ))))
-                           (prog ((r_0 (x_0 ...) g_0) ... (r_1 (x_1 ...) g_1) (r_2 (x_2 ...) g_2) ...) 
-                                  (in-hole Ev (in-hole Es (∂ ((substitute g_1 (x_1 t) ...) σ) #f))))
-                           "Invoke"]
 
                       [==> ((∂ () #f) <-+ s)
                            (∂ s #f)
@@ -100,18 +110,18 @@
                            (∂ ((g σ) <-+ (s × g)) #f)
                            (where #f ,(redex-match? L (⊤ σ) (term s)))
                            "ConjStepAns"]
-                      
-                      [--> (in-hole EΓ (in-hole Ev (∂ (⊤ σ) σ)))
-                           (in-hole EΓ (in-hole Ev ((⊤ σ) + ())))
+
+                      [--> (in-hole Ev (∂ (⊤ σ) σ))
+                           (in-hole Ev ((⊤ σ) + ()))
                            "TopStopAns"]
 
-                      [--> (in-hole EΓ (in-hole Ev (∂ s σ)))
-                           (in-hole EΓ (in-hole Ev ((⊤ σ) + s)))
+                      [--> (in-hole Ev (∂ s σ))
+                           (in-hole Ev ((⊤ σ) + s))
                            (where #f ,(redex-match? L (⊤ σ) (term s)))
                            "TopStepAns"]
 
-                      [--> (in-hole EΓ (in-hole Ev (∂ s #f)))
-                           (in-hole EΓ (in-hole Ev s))
+                      [--> (in-hole Ev (∂ s #f))
+                           (in-hole Ev s)
                            "TopStep"]
 
                       with
@@ -119,13 +129,15 @@
                             (==> a b)]
                       ))
 
-#;(stepper dmitry-and-dmitry '(prog
+#;(stepper dmitry-and-dmitry 
+'(((∃ (x:q) (r:appendo ((sym "dog") : ((sym "cat") : empty)) ((sym "bear") : ((sym "lion") : empty)) x:q "r13") "f12") (state () 0 () "s"))
   ((r:appendo
     (x:l x:s x:out)
     (((x:l =? empty "u2") ∧ (x:out =? x:s "u3") "c1") ∨ (∃ (x:a) (∃ (x:d) (∃ (x:res) (((x:l =? (x:a : x:d) "u9") ∧ (x:out =? (x:a : x:res) "u10") "c8") ∧ (r:appendo x:d x:s x:res "r11") "c7") "f6") "f5") "f4") "d0")))
-  ((∃ (x:q) (r:appendo ((sym "dog") : ((sym "cat") : empty)) ((sym "bear") : ((sym "lion") : empty)) x:q "r13") "f12") (state () 0 () "s"))))
+  ))
 
-#;(stepper dmitry-and-dmitry '(prog
+#;(stepper dmitry-and-dmitry '(
+((∃ (x:a) (∃ (x:b) (r:maino x:a x:b "r9") "f8") "f7") (state () 0 () "s"))
   ((r:m (x:q) ((sym "m") =? (sym "m") "u0"))
    (r:maino
     (x:p x:q)
@@ -136,5 +148,5 @@
       ((sym "m") =? x:p "u6")
       "c2")
      "f1")))
-  ((∃ (x:a) (∃ (x:b) (r:maino x:a x:b "r9") "f8") "f7") (state () 0 () "s")))
+  )
 )
