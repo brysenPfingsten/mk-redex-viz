6a01aa1ffdc35c2f2a4f1141f7c8adbc0252afa4
Jason Hemann <jason.hemann@gmail.com>
2025-09-25 07:45:20 -0400

    Split relation, can't believe this is right though.

--- racket-server/src/reduction-relations/reduction-relations.rkt
+++ racket-server/src/reduction-relations/reduction-relations.rkt
@@ -13,9 +13,37 @@
   (apply-reduction-relation/tag-with-names red (term ,prog)))
 
 (define red
-  (reduction-relation L 
+  (reduction-relation L
                       #:domain (side-condition (name prog p) (judgment-holds (closed-program? prog)))
 
+                      [--> ((in-hole Ev (in-hole Es (proceed ((r_1 t ... o) σ))))
+                            ((r_0 (x_0 ...) g_0) ... (r_1 (x_1 ...) g_1) (r_2 (x_2 ...) g_2) ...))
+                           ((in-hole Ev (in-hole Es ((substitute g_1 (x_1 t) ...) σ)))
+                            ((r_0 (x_0 ...) g_0) ... (r_1 (x_1 ...) g_1) (r_2 (x_2 ...) g_2) ...))
+                           "Substitute Relation Body And Proceed"]
+
+					  [--> ((in-hole Ev (delay s)) ((r_0 (x_0 ...) g_0) ... (r_1 (x_1 ...) g_1) (r_2 (x_2 ...) g_2) ...))
+						   ((in-hole Ev s) ((r_0 (x_0 ...) g_0) ... (r_1 (x_1 ...) g_1) (r_2 (x_2 ...) g_2) ...))
+						   "Invoke Delay"]
+
+					  [--> ((in-hole Ev ((⊤ σ) <-+ s)) Γ)
+						   ((in-hole Ev ((⊤ σ) + s)) Γ)
+						   "Promote Left Answer"]
+
+					  [--> ((in-hole Ev (s +-> (⊤ σ))) Γ)
+						   ((in-hole Ev ((⊤ σ) + s)) Γ)
+						   "Promote Right Answer"]
+
+					  [--> (e_1 Γ)
+                           (e_2 Γ)
+						   (side-condition (not (null? (apply-reduction-relation red-tree (term e_1)))))
+                           (where e_2 ,(car (apply-reduction-relation red-tree (term e_1))))
+							"Lift"]
+					  ))
+
+(define red-tree
+  (reduction-relation L
+                      #:domain e #:arrow ==>
                       [==> ((g_1 ∨ g_2 _) (state sub c trail o))
                            ((g_1 (state sub c trail o)) <-+ (g_2 (state sub c trail ,(symbol->string (gensym)))))
                            "Distribute State Over Disjunction"]
@@ -59,11 +87,11 @@
                       [==> (() <-+ s)
                            s
                            "Prune Left Disjunction Failure"]
-                      
+
                       [==> (s +-> ())
                            s
                            "Prune Right Disjunction Failure"]
-                      
+
                       [==> ((∃ (x ...) g _) (state sub c trail o))
                            ((substitute g ,@(term (fresh-sub c x ...))) 
                                         (state sub (bump c (x ...)) trail o))
@@ -72,12 +100,6 @@
                       [==> ((r_1 t ... o) σ)
                            (delay (proceed ((r_1 t ... o) σ)))
                            "Relation Call And Add Delay"]
-
-                      [--> ((in-hole Ev (in-hole Es (proceed ((r_1 t ... o) σ))))
-                            ((r_0 (x_0 ...) g_0) ... (r_1 (x_1 ...) g_1) (r_2 (x_2 ...) g_2) ...))
-                           ((in-hole Ev (in-hole Es ((substitute g_1 (x_1 t) ...) σ)))
-                            ((r_0 (x_0 ...) g_0) ... (r_1 (x_1 ...) g_1) (r_2 (x_2 ...) g_2) ...))
-                           "Substitute Relation Body And Proceed"]
 
                       [==> ((t_1 =? t_2 o) (state sub c ((t_3 =? t_4 o_1) ...) o_2))
                            (⊤ (state sub_1 c ((t_3 =? t_4 o_1) ... (t_1 =? t_2 o)) o_2))
@@ -96,24 +118,8 @@
                       [==> ((delay s_1) <-+ s_2)
                            (delay (s_1 +-> s_2))
                            "Propagate Delay Through Left Disjunction And Flip"]
-                      
+
                       [==> (s_2 +-> (delay s_1))
                            (delay (s_2 <-+ s_1))
                            "Propagate Delay Through Right Disjunction And Flip"]
-
-                      [--> (prog Γ (in-hole Ev (delay s)))
-                           (prog Γ (in-hole Ev s))
-                           "Invoke Delay"]
-
-					  [--> ((in-hole Ev ((⊤ σ) <-+ s)) Γ)
-						   ((in-hole Ev ((⊤ σ) + s)) Γ)
-						   "Promote Left Answer"]
-
-					  [--> ((in-hole Ev (s +-> (⊤ σ))) Γ)
-						   ((in-hole Ev ((⊤ σ) + s)) Γ)
-						   "Promote Right Answer"]
-
-					  with
-					  [(--> (in-hole Ex a) (in-hole Ex b))
-							(==> a b)]
 					  ))
