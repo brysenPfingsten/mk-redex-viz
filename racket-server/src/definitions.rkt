8d10c299cd44c35e1f54bca3418f358dfc7b1ed5
Jason Hemann <jason.hemann@gmail.com>
2025-11-18 18:24:35 -0500

    Factor out a core language w/o disj or relcalls

--- racket-server/src/definitions.rkt
+++ racket-server/src/definitions.rkt
@@ -19,85 +19,71 @@
 ;; Right now we pun between a succeed node in the language and a
 ;; successful result, with that substitution. Not a sin.
 
-(define-language L
+(define-language Core
   ;--------------------Top Level-------------------------
-  [p (e Γ)]             ; Program
+  [config (Γ Ans s)]    ; Program
+
   [Γ ((r_!_ d g) ...)]  ; Relation Environment w/ distinct relation names
   [d (x_!_ ...)]        ; Distinct variable declarations
-  
-  ;--------------------Expressions-------------------------
-  [e ()               ; Empty Tree / Failure
-     (⊤ σ)            ; Singleton Answer
-     s                ; Search Tree
-     ((⊤ σ) + e)]     ; Answer Stream
+  [Ans (σ ...)]
 
   ;-------------------Search Trees------------------------
-  [s ()                         ; Empty Tree / Failure
+  [s (empty-tree)               ; Empty Tree / Failure
      (g σ)                      ; Goal-State
-     (∂ s maybe-state)          ; Partially Evaluated Tree
-     (s +-> s)                  ; Right Disjunciton
-     (s <-+ s)                  ; Left Disjunction
-     ((⊤ σ) + s)                ; Answer Stream
      (s × g)                    ; Conjunction
-     (proceed ((r t ... o) σ))  ; Proceed
-     (delay s)]                 ; Delay
+
+     ;; ((⊤ σ) + s)                ; Answer Stream
+
+     ;; (s +-> s)                  ; Right Disjunciton
+     ;; (s <-+ s)                  ; Left Disjunction
+     ;; (proceed ((r t ... o) σ))  ; Proceed
+     ;; (delay s)
+
+  ]                 ; Delay
 
   ;----------------------Goals----------------------------
-  [g ⊤
-     (t =? t o)    ; Syntactic equality w/ tag
-     (r t ... o)   ; Relation call w/ tag
-     (g ∨ g o)     ; Disjunction w/ tag
-     (g ∧ g o)     ; Conjunction w/ tag
-     (∃ d g o)]    ; Variable introduction w/ tag
+  [eq (t =? t tag)] ; Syntactic equality w/ tag
 
+  [g eq
+     (succeed)
+     (∃ d g tag)     ; Variable introduction w/ tag
+     (g ∧ g tag)     ; Conjunction w/ tag
+     ;(r t ... tag)   ; Relation call w/ tag
+     ;(g ∨ g tag)     ; Disjunction w/ tag
+
+  ]
   ;----------------------Terms---------------------------
-  [t c              ; Logic variables
-     (sym string)   ; String
-     (nat natural)  ; Naturals
+  [t x             ; Parameters
+     u             ; logic vars
+     (sym string)  ; String contants
+     (nat number)  ; Numeric constants
      boolean
      string
-     x        ; Parameters
-     empty    ; Empty list
-     (t : t)] ; Non-empty list
+     empty         ; Empty list
+     (t : t)       ; Non-empty list
+   ]
 
 
   ;----------------------Other---------------------------
   [r (variable-prefix r:)]  ; to account for arbitrary relation names
   [x (variable-prefix x:)]  ; to account for arbitrary parameter names
-  [c natural]               ; Logic variables
-  [o (sym string)           ; Tagged string  = symbol
-     (nat natural)          ; Tagged natural = natural (not logic variable)
-     boolean
-     string]
-  [σ (state sub c trail o)] ; State
-  [sub ((natural t) ...)]   ; Substitution
+  [u (variable-prefix u:)]  ; Logic variables – tagged, not raw naturals
+  [tag (label string)]
+
+  [σ (state sub c trail tag)] ; State
+  [sub ((u t) ...)]   ; Substitution
   [maybe-sub sub #f]
-  [maybe-state σ #f]
-  [trail ((t =? t o) ...)]
-
-
-  ;----------------------Values--------------------------
-  [v ()           ; Empty Node
-     (⊤ σ)        ; Singleton Node
-     ((⊤ σ) + v)] ; Answer Disjunct
-
-  [prog-val (prog Γ v)]
-
-
+  [trail (eq ...)]
+  [end-config (Γ Ans (empty-tree))]
+  [c natural]
   ;-----------------Evaluation Contexts------------------
-
-  ; Answer Stream
-  [Ev hole
-      ((⊤ σ) + Ev)]
 
   ; Search Tree
   [Es hole
-      (Es <-+ s)
-      (s +-> Es)
-      (Es × g)]
-
-  ;; Prog to first tree w/o sub-tree
-  [Ex (in-hole Ev (in-hole Es hole))]
+      (Es × g)
+      ;; (Es <-+ s)
+      ;; (s +-> Es)
+  ]
 
   ;---------------------Binding Forms--------------------
   #:binding-forms
@@ -105,38 +91,39 @@
   (e #:refers-to (shadow r ...) ((r (x ...) g #:refers-to (shadow x ...)) ...) #:refers-to (shadow r ...))
 )
 
-(default-language L)
+(default-language Core)
 
-(define-metafunction L
+(define-metafunction Core
   unify : t t sub -> maybe-sub
-  [(unify natural_1 natural_1 sub) sub]
-  [(unify natural t sub) (extend natural t sub)]
-  [(unify t natural sub) (extend natural t sub)]
+  [(unify u_1 u_1 sub) sub]
+  [(unify u t sub) (extend u t sub)]
+  [(unify t u sub) (extend u t sub)]
   [(unify (t_1a : t_1b) (t_2a : t_2b) sub)
    (unify (walk t_1b sub_1) (walk t_2b sub_1) sub_1)
    (where sub_1 (unify (walk t_1a sub) (walk t_2a sub) sub))]
   [(unify t_1 t_1 sub) sub]
   [(unify _ _ _) #f])
 
-(define-metafunction L
+(define-metafunction Core
   walk : t sub -> t
-  [(walk natural (name sub (_ ... [natural t] _ ...))) (walk t sub)]
+  [(walk u (name sub (_ ... [u t] _ ...))) (walk t sub)]
   [(walk t _) t])
-  
-(define-metafunction L
-  extend : natural t sub -> maybe-sub
-  [(extend natural t sub) ([natural t] ,@(term sub))
-                          (side-condition (not (term (occurs? natural t sub))))]
+
+(define-metafunction Core
+  extend : u t sub -> maybe-sub
+  [(extend u t sub) ([u t] ,@(term sub))
+                          (side-condition (not (term (occurs? u t sub))))]
   [(extend _ _ _) #f])
 
-(define-metafunction L
-  fresh-sub : c any ... -> any
+  ;; produce the mapping between lexical vars an the numbers for logic vars
+(define-metafunction Core
+  fresh-sub : c x ... -> ((x c) ...)
   [(fresh-sub c) ()]
   [(fresh-sub c x_1 x_2 ...)
    ,(cons (term (x_1 c)) (term (fresh-sub ,(add1 (term c)) x_2 ...)))])
 
-(define-relation L
-  occurs? ⊆ natural × t × sub
-  [(occurs? natural (t : _) sub) (occurs? natural t sub)]
-  [(occurs? natural (_ : t) sub) (occurs? natural t sub)]
-  [(occurs? natural_1 natural_1 sub)])
+(define-relation Core
+  occurs? ⊆ u × t × sub
+  [(occurs? u (t : _) sub) (occurs? u t sub)]
+  [(occurs? u (_ : t) sub) (occurs? u t sub)]
+  [(occurs? u_1 u_1 sub)])
